1e854ebc284bb8e86e204bdfcab07e5874a97a53
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-03-22 01:03:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,9 @@
 class Dog 
   def name=(dog_name)
-    @dog_name = dog_name
+    @this_dog_name = dog_name
   end
-end
 
-  def breed=(dog_breed)
-    @dog_breed=(dog_breed)
+def name
+   @this_dog_name
   end
 end
